fa3166f6cb9e9d2a7d3cf97785b5ae4cdd6c4771
jacobmorency <jvmorency@gmail.com>
2026-03-13 02:19:00 -0400

    create basic test routes for workouts and exercises

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
-const express = require("express");
+import express from "express";
+import exerciseRoutes from "./routes/exercises.js";
+import workoutRoutes from "./routes/workouts.js";
 
 const app = express();
 
-module.exports = app;
+app.use("/exercises", exerciseRoutes);
+app.use("/workouts", workoutRoutes);
+
+export default app;
