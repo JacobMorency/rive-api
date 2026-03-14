510dfdf10ee2627dcaef98a591e49e74c866eacf
jacobmorency <jvmorency@gmail.com>
2026-03-14 17:00:48 -0400

    remove workout routes to simplify learning

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
 import express, { Application } from "express";
 import exerciseRoutes from "./routes/exercises.js";
-import workoutRoutes from "./routes/workouts.js";
 
 const app: Application = express();
 
 app.use("/exercises", exerciseRoutes);
-app.use("/workouts", workoutRoutes);
 
 export default app;
--- src/routes/workouts.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from "express";
-
-const router: Router = Router();
-
-router.get("/", (req: Request, res: Response) => {
-  res.send("workouts here");
-});
-
-export default router;
