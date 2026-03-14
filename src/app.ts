34acc1e647c83f79543677b92089aa72b1a5fdaf
jacobmorency <jvmorency@gmail.com>
2026-03-14 18:29:35 -0400

    created simple errorHandler.ts middlware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express, { Application } from "express";
 import exerciseRoutes from "./routes/exercises.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 const app: Application = express();
 
 app.use("/exercises", exerciseRoutes);
+
+app.get("/error-test", (req, res) => {
+  throw new Error("Oops");
+});
+
+app.use(errorHandler);
 
 export default app;
